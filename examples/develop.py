ddab801cfc6deb9fe23cbdb420b2301bca4681c7
DeepSmashProject <deepsmash0@gmail.com>
2021-11-12 14:48:44 +0900

    initial commit

--- examples/develop.py
+++ examples/develop.py
@@ -8,7 +8,7 @@ controller = Controller()
 def callback(frame, fps):
     print("callback!", frame[0][0], fps)
     start = time.time()
-    controller.press(Action.ACTION_JAB)
+    controller.act(Action.ACTION_JAB)
     elapsed_time = time.time() - start
     print(elapsed_time)
 screen = Screen(callback, fps=60)
