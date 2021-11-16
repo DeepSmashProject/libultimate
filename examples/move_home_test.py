12b7566f521974f97ed350eec9a3e030403a023a
DeepSmashProject <deepsmash0@gmail.com>
2021-11-16 17:45:05 +0900

    add test

---
+++ examples/move_home_test.py
@@ -0,0 +1,9 @@
+from yuzulib import Runner, Screen
+from libultimate import Controller, Action, TrainingMode, Stage, Fighter
+import time
+import random
+#runner = Runner("", "")
+#runner.run()
+
+controller = Controller()
+controller.move_to_home()
