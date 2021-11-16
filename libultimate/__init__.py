b8634695ec363595321cd6d8066ffd33ac37236e
DeepSmashProject <deepsmash0@gmail.com>
2021-11-16 14:08:33 +0900

    export yuzulib

--- libultimate/__init__.py
+++ libultimate/__init__.py
@@ -1,6 +1,5 @@
+from yuzulib import Runner, Screen
 from .controller import Controller
 from .enums import Action, Stage, Fighter
 from .mode import Mode, TrainingMode
-from .runner import Runner
-from .screen import Screen
 from .util import click_mouse, press_key, move_mouse
