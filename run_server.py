6abf6789b71ddcd6b6406ba5007f24328f177709
DeepSmashProject <deepsmash0@gmail.com>
2021-12-15 23:30:27 +0900

    add serverview

--- run_server.py
+++ run_server.py
@@ -1,11 +1,11 @@
 import argparse
 from libultimate.server import ScreenView, UltimateControllerView
-from yuzulib.server import Server, ControllerView, RunnerView
+from yuzulib.server import Server, ControllerView, RunnerView, ServerView
 parser = argparse.ArgumentParser(description='')
 parser.add_argument('--host')
 parser.add_argument('--port')
 
 args = parser.parse_args()
 print("Running Ultimate Server at {}:{}".format(args.host, args.port))
-server = Server(host=str(args.host), port=int(args.port), views=[ControllerView, RunnerView, ScreenView, UltimateControllerView])
+server = Server(host=str(args.host), port=int(args.port), views=[ControllerView, RunnerView, ScreenView, UltimateControllerView, ServerView])
 server.run()
