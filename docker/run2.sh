4259d80722dff55fb8a52fa5e00d3cf33bd480de
DeepSmashProject <deepsmash0@gmail.com>
2021-12-19 17:18:00 +0900

    update Dockerfile

--- docker/run2.sh
+++ docker/run2.sh
@@ -10,7 +10,9 @@ docker run --privileged -it --rm --gpus all \
   -e API_PORT=6000 \
   -e API_HOST=0.0.0.0 \
   -e API_FILE_PATH=/workspace/libultimate/run_server.py \
-  #-v "/home/ruirui_nis/workspace/DeepSmashProject:/workspace" \
   -v "/mnt/bigdata/00_students/ruirui_nis/DeepSmashProject/games:/workspace/games" \
   -v "/mnt/bigdata/00_students/ruirui_nis/DeepSmashProject/keys:/root/.local/share/yuzu/keys" \
   --name libultimate deepsmash/libultimate
+
+
+  #-v "/home/ruirui_nis/workspace/DeepSmashProject:/workspace" \
