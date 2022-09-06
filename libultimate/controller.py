9778607bb10cb980aea292e7ae035f5f25ff53a2
DeepSmashProject <deepsmash0@gmail.com>
2022-09-06 13:49:37 +0900

    add stick in python controller

--- libultimate/controller.py
+++ libultimate/controller.py
@@ -8,16 +8,20 @@ class Command(NamedTuple):
     id: str
     player_id: int
     action: Action
+    stick_x: float
+    stick_y: float
 
 
 class UltimateController:
     def __init__(self, console: Console):
         self.console = console
 
-    def act(self, player_id: int, action: Action):
+    def act(self, player_id: int, action: Action, stick_x: float=0, stick_y: float=0):
         command: Command = {
             "id": str(uuid.uuid4()),
             "player_id": player_id,
-            "action": action
+            "action": action,
+            "stick_x": stick_x,
+            "stick_y": stick_y,
         }
         self.console.api.send_command(player_id, command)
