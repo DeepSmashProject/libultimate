b48425b03d57d26f31d59431547c47123a43b234
DeepSmashProject <deepsmash0@gmail.com>
2022-09-11 18:05:17 +0900

    update env

--- libultimate/env.py
+++ libultimate/env.py
@@ -1,16 +1,47 @@
-from libultimate import Controller, Console
+from libultimate import Controller, Console, Button, Direction
+from enum import Enum
 import gym
 import time
 import threading
 
-action_list_default = [
-]
+class EnvAction(Enum):
+    NONE = 0
+    JAB = lambda cont: cont.jab()
+    TILT_U = lambda cont: cont.tilt(Direction.UP)
+    TILT_L = lambda cont: cont.tilt(Direction.LEFT)
+    TILT_D = lambda cont: cont.tilt(Direction.DOWN)
+    TILT_R = lambda cont: cont.tilt(Direction.RIGHT)
+    SMASH_U = lambda cont: cont.smash(Direction.UP)
+    SMASH_L = lambda cont: cont.smash(Direction.LEFT)
+    SMASH_D = lambda cont: cont.smash(Direction.DOWN)
+    SMASH_R = lambda cont: cont.smash(Direction.RIGHT)
+    SPECIAL_U = lambda cont: cont.special(Direction.UP)
+    SPECIAL_L = lambda cont: cont.special(Direction.LEFT)
+    SPECIAL_D = lambda cont: cont.special(Direction.DOWN)
+    SPECIAL_R = lambda cont: cont.special(Direction.RIGHT)
+    DASH_ATTACK_L = lambda cont: cont.dash_attack(lr=False)
+    DASH_ATTACK_R = lambda cont: cont.dash_attack(lr=True)
+    SPOT_DODGE = lambda cont: cont.spot_dodge()
+    ROLL_L = lambda cont: cont.roll(lr=False)
+    ROLL_R = lambda cont: cont.roll(lr=True)
+    JUMP = lambda cont: cont.jump()
+    JUMP_L = lambda cont: cont.jump(lr=False)
+    JUMP_R = lambda cont: cont.jump(lr=True)
+    SHORT_HOP = lambda cont: cont.short_hop()
+    SHORT_HOP_L = lambda cont: cont.short_hop(lr=False)
+    SHORT_HOP_R = lambda cont: cont.short_hop(lr=True)
+    WALK_L = lambda cont: cont.walk(lr=False)
+    WALK_R = lambda cont: cont.walk(lr=True)
+    DASH_L = lambda cont: cont.dash(lr=False)
+    DASH_R = lambda cont: cont.dash(lr=True)
+    GUARD = lambda cont: cont.guard()
+    GRAB = lambda cont: cont.grab()
 
 class UltimateEnv(gym.Env):
-    def __init__(self, console: Console, controller: Controller, hz=60, action_list=action_list_default):
+    def __init__(self, console: Console, controller: Controller, hz=60, action_space=int(len(Action))):
         super().__init__()
         self.hz = hz
-        self.action_space = gym.spaces.Discrete(len(action_list)) 
+        self.action_space = gym.spaces.Discrete(action_space) 
         self.console = console
         self.controller = controller
         self.gamestate = None
@@ -32,14 +63,15 @@ class UltimateEnv(gym.Env):
 
     def reset(self, without_reset=False):
         if not without_reset:
-            self.controller.mode.training.reset()
+            self.controller.input([Button.L, Button.R, Button.A])
         observation = self._gamestate_to_observation(self.gamestate)
         self.controller.release_all()
         time.sleep(1)
         return observation
 
     def step(self, action):
-        self.controller.act(action)
+        action.value(self.controller)
+        #self.controller.act(action)
         interval = 60/self.hz * (1/60)
         time.sleep(interval)
         observation = self._gamestate_to_observation(self.gamestate)
@@ -50,15 +82,10 @@ class UltimateEnv(gym.Env):
         return observation, reward, self.done, info
 
     def render(self, mode='human', close=False):
-        if mode == 'human':
-            print("You can see screen at http://localhost:8081/vnc.html")
-        else:
             print("GameState: {}".format(self.gamestate))
 
     def _done(self, gamestate, prev_gamestate):
-        if (prev_gamestate.players[0].percent != 0 and gamestate.players[0].percent == 0) or (prev_gamestate.players[1].percent != 0 and gamestate.players[1].percent == 0):
-            return True
-        return False
+        return prev_gamestate.players[0].is_dead or prev_gamestate.players[1].is_dead
 
     def _reward(self, done, gamestate, prev_gamestate):
         p1_diff_damage = gamestate.players[0].percent - prev_gamestate.players[0].percent
