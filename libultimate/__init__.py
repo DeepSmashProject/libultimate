6b8e7a7f9330e8a97d20c4bd2f9fa9665ca0fe25
DeepSmashProject <deepsmash0@gmail.com>
2022-09-11 18:05:17 +0900

    update env

--- libultimate/__init__.py
+++ libultimate/__init__.py
@@ -4,4 +4,4 @@ from .api import API
 from .enums import Stage, Fighter, Button, Direction, Action
 from .gamestate import GameState, Position, PlayerState, Projectile
 from .mode import Mode
-from .env import UltimateEnv
+from .env import UltimateEnv, EnvAction
