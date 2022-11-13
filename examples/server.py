6ef4a0d5c76eace8ffda2a83e54799cd6d3b448a
DeepSmashProject <deepsmash0@gmail.com>
2022-11-13 18:43:56 +0900

    update server

--- examples/server.py
+++ examples/server.py
@@ -1,7 +1,8 @@
-from libultimate import UltimateServer, Console
+from libultimate import UltimateServer, UltimateServerConfig, Console
 
 if __name__ == "__main__":
     RYUJINX_PATH = "/path/to/Ryujinx" # ex: /home/username/.config/Ryujinx
     with Console(ryujinx_path=RYUJINX_PATH) as console:
-        server = UltimateServer(console)
+        config = UltimateServerConfig(fps=5)
+        server = UltimateServer(console, config)
         server.run(address="localhost", port=8000)
--- libultimate/__init__.py
+++ libultimate/__init__.py
@@ -5,4 +5,4 @@ from .enums import Stage, Fighter, Button, Direction, Action
 from .gamestate import GameState, Position, PlayerState, Projectile
 from .mode import Mode
 from .env import UltimateEnv, EnvAction
-from .server import UltimateServer
+from .server import UltimateServer, UltimateServerConfig
