b83f1920f619fb48774b7739e3dd2457c71614c2
DeepSmashProject <deepsmash0@gmail.com>
2022-11-14 18:57:20 +0900

    update Dockerfile

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,5 +2,7 @@
 # 0. generate xorg.conf
 sudo nvidia-xconfig -a --allow-empty-initial-configuration --enable-all-gpus
 
+cp /home/default/data/keys/prod.keys /home/default/.config/Ryujinx/system/prod.keys
+
 # for running systemd
-/sbin/init
+#/sbin/init
