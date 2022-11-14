9239eb4ee572d2dae23ad97be677cdad705914c6
DeepSmashProject <deepsmash0@gmail.com>
2022-11-14 12:45:04 +0900

    fix entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,13 +1,6 @@
 
-   
-# inside docker script
-trap 'kill $(jobs -p)' EXIT
-
 # 0. generate xorg.conf
-# This is for OpenGL 4.6.0
-BUS_ID=$(nvidia-xconfig --query-gpu-info | grep 'PCI BusID' -m 1 | sed -r 's/\s*PCI BusID : PCI:(.*)/\1/')
-echo "BUS_ID=$BUS_ID"
-sudo nvidia-xconfig -a --virtual=$RESOLUTION --allow-empty-initial-configuration --enable-all-gpus --busid $BUS_ID
+sudo nvidia-xconfig -a --allow-empty-initial-configuration --enable-all-gpus
 
-# bash
-/bin/bash
+# for running systemd
+/sbin/init
