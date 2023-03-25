f12490bcd9a55506cc49c1538828d3d80752cc43
RuiHirano <r.hrn.0930@gmail.com>
2023-03-25 14:07:48 +0900

    fix docker file

--- examples/client.py
+++ examples/client.py
@@ -1,8 +1,18 @@
 from libultimate import UltimateClient, Button
 import json
+import cv2
 if __name__ == "__main__":
     client = UltimateClient("http://localhost:8008")
     client.add_controller(0)
-    for gamestate in client.stream(fps=5, include_image=True, image_size=(256, 256)):
+    for gamestate in client.stream(fps=5, include_image=True, image_size=(84, 84)):
         print("gamestate: ", gamestate.players[0].position, gamestate.image.shape)
-        client.input(0, [Button.A])
+        cv2.imshow('camera' , gamestate.image)
+
+        #繰り返し分から抜けるためのif文
+        key =cv2.waitKey(10)
+        if key == 27:
+            break
+        #client.input(0, [Button.A])
+        
+#メモリを解放して終了するためのコマンド
+cv2.destroyAllWindows()
